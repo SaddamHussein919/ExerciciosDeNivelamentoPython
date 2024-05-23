28cabb63060bcb27cc67e8d480246e9ba7a43fa9
SaddamHussein919 <137503910+SaddamHussein919@users.noreply.github.com>
2024-05-23 10:58:59 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,66 @@
+nome = "Gabriel"
+idade = 18
+salário = 1400
+cargo = "repositor"
+turno = "noturno"
+setor = 5
+
+nome = "Colégio José Balan"
+estado = "Paraná"
+numeroDeProfessores = 25
+cidade = "Umuarama"
+numeroDeMilitares = 50
+logradouro = "Exemplo de logradouro"
+endereço = "Exemplo de endereço"
+numeroDeAlunos = 100
+colegioEMilitar = True
+disciplinas = "Português, Matemática, Geografia, História"
+
+valor1 = 10
+valor2 = 5
+valor3 = 2
+valor4 = 8
+valor5 = -5
+soma = valor1 + valor2
+print(soma)
+soma = valor1 + valor2 + valor4
+print(soma)
+soma = valor1 + valor2 - valor5
+print(soma)
+soma = valor1 + valor3
+print(soma)
+soma = valor1 * valor2
+print(soma)
+soma = (valor4 * valor2) / 2
+print(soma)
+soma = (valor1 + valor2 + valor4 + valor5) / 4
+print(soma)
+
+v1 = 2
+v2 = 3
+v3 = 5
+v4 = 6
+somaDosValores = v1 + v2 + v3 + v4
+print(somaDosValores)
+mediaDosValores = somaDosValores / 4
+print(mediaDosValores)
+
+nota1 = 64
+nota2 = 45
+nota3 = 60
+somaDasNotas = nota1 + nota2 + nota3
+mediaDasNotas = somaDasNotas / 3
+if(mediaDasNotas >= 60):
+    print("Aprovado")
+else:
+    print("Reprovado")
+
+mediaAluno = 60
+alunoEmCurso = True
+if(True, mediaAluno >= 60):
+    print("Aprovado com certificado")
+elif(False, mediaAluno >=60):
+    print("Aprovado")
+else:
+    print("Reprovado")
+
